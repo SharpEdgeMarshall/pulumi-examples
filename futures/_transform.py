f96472723d3c690b28e2ee2e4eb3d8fb058521d5
Fabio Todaro <fabio.todaro@prima.it>
2024-05-29 15:41:49 +0200

    Add format

--- futures/_transform.py
+++ futures/_transform.py
@@ -40,6 +40,16 @@ def concat_example():
     pulumi.export("my_id", my_id.hex)
 
 
+def format_example():
+    my_str = RandomString("my-random-str", length=5)
+    my_prefix = pulumi.Output.format("BEFORE-{0}-AFTER", my_str.result)
+
+    my_id = RandomId("my-random-id", byte_length=8, prefix=my_prefix)
+
+    pulumi.export("my_str", my_str.result)
+    pulumi.export("my_id", my_id.hex)
+
+
 def from_input_example(my_str: pulumi.Input[str]):
 
     my_prefix = pulumi.Output.from_input(my_str).apply(
@@ -64,6 +74,7 @@ def wrong_way():
 apply_example()
 # all_example()
 # concat_example()
+# format_example()
 # from_input_example(RandomString("my-random-str", length=5).result)
 # from_input_example("mystr")
 # wrong_way()
